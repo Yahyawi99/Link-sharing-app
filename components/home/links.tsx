121a1f2ea0c132676f49a52bd84217adfa93b15a
Yahyawi99 <yassinyahyawi26@gmail.com>
2024-03-21 18:52:43 +0000

    working on home page

--- components/home/links.tsx
+++ components/home/links.tsx
@@ -1,3 +1,34 @@
+import styles from "@/styles/pages/home/links.module.css";
+
 export default async function Links() {
-  return <div>LINKS</div>;
+  return (
+    <div className={styles.linksContainer}>
+      <h1>Customize your links</h1>
+      <p>
+        Add/edit/remove links below and then share all your profiles with the
+        world
+      </p>
+
+      <button>+ Add new link</button>
+
+      <form className={styles.links}>
+        <div>
+          <div>
+            <p> = Link #1</p>
+            <button>remove</button>
+          </div>
+
+          <div>
+            <label htmlFor="url">Link</label>
+            <input
+              type="text"
+              id="url"
+              name="url"
+              placeholder="e.g., https://www.example.com/username"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
