5de1fc817bf823eb37404731a61be304f03af668
Yahyawi99 <yassinyahyawi26@gmail.com>
2024-03-27 14:03:54 +0000

    working on profile details

--- components/home/links.tsx
+++ components/home/links.tsx
@@ -127,8 +127,8 @@ function Empty() {
       <h1>Let's get you started</h1>
       <p>
         Use the “Add new link” button to get started. Once you have more than
-        one link, you can reorder and edit them. We’re here to help you share
-        your profiles with everyone!
+        one link, you can edit them. We’re here to help you share your profiles
+        with everyone!
       </p>
     </div>
   );
--- components/home/profile-details.tsx
+++ components/home/profile-details.tsx
@@ -1,3 +1,52 @@
+import styles from "@/styles/components/home/profileDetails.module.css";
+
 export default async function ProfileDetails() {
-  return <div>PROFILE DETAILS</div>;
+  return (
+    <div className={styles.profileDetailsContainer}>
+      <h1>Profile Details</h1>
+      <p>Add your details to create a personal touch to your profile.</p>
+
+      <form action="">
+        <div>
+          <p>Profile picture</p>
+
+          <input type="file" accept=".png, .jpg, .jpeg" name="avatar" />
+
+          <p>Image must be below 1024x1024px. Use PNG or JPG format.</p>
+        </div>
+
+        <div>
+          <div>
+            <label htmlFor="">
+              First name
+              <span>*</span>
+            </label>
+            <input type="text" name="firstName" placeholder="e.g Yassin" />
+          </div>
+
+          <div>
+            <label htmlFor="">Last name</label>
+            <input type="text" name="lastName" placeholder="e.g Yahyawi" />
+          </div>
+
+          <div>
+            <label htmlFor="">
+              Email
+              <span>*</span>
+            </label>
+            <input
+              type="text"
+              name="email"
+              placeholder="e.g yassin@gmail.com"
+            />
+          </div>
+        </div>
+
+        <div className={styles.submitBtn}>
+          <button type="submit">Save</button>
+          {/* <p className={styles.error}>{formState.errors?.join("")}</p> */}
+        </div>
+      </form>
+    </div>
+  );
 }
