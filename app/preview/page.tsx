34bb6ebce9111126f89196e48d112e5aff5237cf
Yahyawi99 <yassinyahyawi26@gmail.com>
2024-04-01 16:20:16 +0000

    working on share link

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useMain } from "@/context";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import PhoneCardPreview from "@/components/preview/phone-card";
