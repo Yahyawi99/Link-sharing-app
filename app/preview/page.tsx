0c783dd27870961b61bef4a14388ccd2e8b1277d
Yahyawi99 <yassinyahyawi26@gmail.com>
2024-04-01 16:10:22 +0000

    working on share link

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,8 +1,26 @@
+"use client";
+
+import { useMain } from "@/context";
+import { useRef, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import PhoneCardPreview from "@/components/preview/phone-card";
 import styles from "@/styles/pages/preview/index.module.css";
 
-export default async function Preview() {
+export default function Preview() {
+  const { user } = useMain();
+  const [copied, setCopied] = useState(false);
+
+  const shareLink = () => {
+    const link = `${window.location.origin}/user/${user?.id}`;
+    navigator.clipboard.writeText(link);
+
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
+
   return (
     <div className={styles.container}>
       <div>
@@ -11,11 +29,16 @@ export default async function Preview() {
             <button>Back to Editor</button>
           </Link>
 
-          <button>Share Link</button>
+          <button onClick={shareLink}>Share Link</button>
         </header>
       </div>
 
       <PhoneCardPreview />
+
+      <div className={`${styles.copyModal} ${copied ? styles.copied : ""}`}>
+        <Image src="/icons/icon-link.svg" alt="chain" width={25} height={25} />
+        <p>The link has been copied to your clipboard</p>
+      </div>
     </div>
   );
 }
